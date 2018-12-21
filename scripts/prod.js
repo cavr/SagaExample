6dcc839fc28177fb9f9a5b4900aaf022ea5ba5d8
Charles <amiguero809@gmail.com>
2018-12-21 13:01:05 +0100

    Add HEROKU deploy

--- scripts/prod.js
+++ scripts/prod.js
@@ -8,7 +8,7 @@ app.get( '/*', function ( request, response ) {
     response.sendFile( path.join( __dirname, '../build', 'index.html' ) );
 } );
 
-const port = 443;
+const port = process.env.PORT || 3000
 
 
 app.listen({ port }, () =>
