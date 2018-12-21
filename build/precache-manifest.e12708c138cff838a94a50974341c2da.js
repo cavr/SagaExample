fc9869f05b130899dd7d5ec0fc4d60b001f9a434
Charles <amiguero809@gmail.com>
2018-12-21 14:05:28 +0100

    Add Build

---
+++ build/precache-manifest.e12708c138cff838a94a50974341c2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b3828c8ffa96a25f99a",
+    "url": "/static/js/main.9b3828c8.chunk.js"
+  },
+  {
+    "revision": "d23291e8801c561058e0",
+    "url": "/static/js/1.d23291e8.chunk.js"
+  },
+  {
+    "revision": "9b3828c8ffa96a25f99a",
+    "url": "/static/css/main.1f0dd6d5.chunk.css"
+  },
+  {
+    "revision": "ace22fc4793e8a1aebb1c25e9df2ea0d",
+    "url": "/index.html"
+  }
+];
